ce48f49d6f1465b6e22742ae54608c5cb7885e49
MD. IIMTIAZ KHAN SHAFE <50952183+iimtiazkhanshafe@users.noreply.github.com>
2020-07-22 20:58:24 +0600

    Update How many bits you have hit on lottery.py

--- How many bits you have hit on lottery.py
+++ How many bits you have hit on lottery.py
@@ -1,13 +1,13 @@
 bits = [10, 1, 2] # edit this
-down = [11] # edit this
+drawn = [11] # edit this
 hits = 0
 for number in bits:
-    if number in down:
+    if number in drawn:
         hits += 1
 
 nothits = 0
 for number in bits:
-    if number not in down:
+    if number not in drawn:
         nothits += 1
         
 print("You hits : ", hits)
